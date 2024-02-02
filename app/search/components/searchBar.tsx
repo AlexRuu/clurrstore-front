3e03550ce7500c9a4a15224c5d2709a0a09ad9b0
Alex <weiru@hotmail.ca>
2024-02-02 18:44:44 -0500

    Create search page

---
+++ app/search/components/searchBar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import SearchForm from "@/components/forms/search-form";
+
+const SearchPageBar = () => {
+  return <SearchForm />;
+};
+
+export default SearchPageBar;
---
+++ app/search/page.tsx
@@ -0,0 +1,11 @@
+import SearchPageBar from "./components/searchBar";
+
+const searchPage = () => {
+  return (
+    <div>
+      <SearchPageBar />
+    </div>
+  );
+};
+
+export default searchPage;
