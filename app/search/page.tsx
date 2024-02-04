f007d417cee6f523651e3139b1a3b731ecc99a78
Alex <weiru@hotmail.ca>
2024-02-03 20:39:17 -0500

    Format search page to match others

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -14,21 +14,25 @@ const searchPage = async ({
 
   console.log(searchProducts);
   return (
-    <div>
-      <PageHeader second="search" headerTitle="Search" />
-      <SearchPageBar />
-      {searchProducts.length > 0 ? (
-        <div>
-          {searchProducts.map((product) => (
-            <ProductCard data={product} />
-          ))}
-        </div>
-      ) : (
-        <div>
-          <h1>No results found...</h1>
-        </div>
-      )}
-    </div>
+    <main className="min-h-[500px] mt-10 med-small:mx-5">
+      <section className="xl:!p-[0px_85px] p-[0px_55px] med-small:p-0">
+        <PageHeader second="search" headerTitle="Search" />
+        <SearchPageBar />
+        {searchProducts.length > 0 ? (
+          <div className="flex w-full">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 box-border w-full">
+              {searchProducts.map((product) => (
+                <ProductCard data={product} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div>
+            <h1>No results found...</h1>
+          </div>
+        )}
+      </section>
+    </main>
   );
 };
 
