201e776310975314d172949578fa36774f87df8a
Alex <weiru@hotmail.ca>
2024-02-08 15:29:18 -0500

    Update font to Poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,14 @@ import Navbar from "@/components/ui/navbar/navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import PreviewModalProvider from "@/providers/preview-modal-provider";
+import { Poppins } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Clurr's Studio",
   description: "All things aesthetic and cute",
 };
+
+const font = Poppins({ weight: "500", subsets: ["devanagari"] });
 
 export default async function RootLayout({
   children,
@@ -15,7 +18,7 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en-us">
-      <body>
+      <body className={font.className}>
         <div className="max-w-[100vw] overflow-x-hidden w-full">
           <Navbar />
           <PreviewModalProvider />
