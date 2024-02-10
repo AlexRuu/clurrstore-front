a281183acaa5aa44f6d293c53f17695f3f0772cd
Alex <weiru@hotmail.ca>
2024-02-09 23:08:55 -0500

    Add light poppins font

--- app/font.ts
+++ app/font.ts
@@ -5,6 +5,11 @@ export const poppins = Poppins({
   weight: "500",
   subsets: ["devanagari", "latin"],
 });
+
+export const poppins_light = Poppins({
+  weight: "400",
+  subsets: ["devanagari", "latin"],
+});
 export const yeseva_one = Yeseva_One({
   weight: ["400"],
   subsets: ["latin", "cyrillic"],
