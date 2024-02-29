acb2707f90682c27d6bb3a6423c89d57e2260496
Alex <weiru@hotmail.ca>
2024-02-29 16:31:01 -0500

    Create order header component

---
+++ app/order/[orderNumber]/components/order-header.tsx
@@ -0,0 +1,35 @@
+"use client";
+import useCart from "@/hooks/use-cart";
+import { CheckCircle2 } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+interface OrderHeaderProps {
+  orderNumber: string;
+}
+
+const OrderHeader: React.FC<OrderHeaderProps> = ({ orderNumber }) => {
+  const searchParams = useSearchParams();
+  const removeAll = useCart((state) => state.removeAll);
+
+  useEffect(() => {
+    if (searchParams.get("success")) {
+      removeAll();
+    }
+  }, [searchParams, removeAll]);
+
+  return (
+    <>
+      <div>
+        <CheckCircle2 size={64} />
+      </div>
+      <div className="flex flex-col justify-center items-center flex-nowrap">
+        <h1 className="text-4xl mt-5 my-3">Order Confirmed!</h1>
+        <p>Thank you for your support!</p>
+        <p>Your order number is #{orderNumber}</p>
+      </div>
+    </>
+  );
+};
+
+export default OrderHeader;
