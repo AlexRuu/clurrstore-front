3a144edf995fcce9b11b3befe24db94c375c3232
Alex <weiru@hotmail.ca>
2024-03-09 00:01:32 -0500

    Fetch profile by id and not email

--- actions/get-profile.tsx
+++ actions/get-profile.tsx
@@ -2,8 +2,8 @@ import { Profile } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/profile`;
 
-const getProfile = async (email: string): Promise<Profile> => {
-  const res = await fetch(`${URL}/${email}`);
+const getProfile = async (id: string): Promise<Profile> => {
+  const res = await fetch(`${URL}/${id}`);
 
   return res.json();
 };
