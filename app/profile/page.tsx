86191e948e1a7724e6e539d69ba9cb6ea852caae
Alex <weiru@hotmail.ca>
2024-03-09 20:21:44 -0500

    Add meta data and title

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,6 +4,13 @@ import { createClient } from "@/libs/supabase/server";
 import ProfileInfo from "./components/profile-info";
 import getProfile from "@/actions/get-profile";
 import ProfileOrderInfo from "./components/order-info";
+import { Metadata } from "next";
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: `Profile`,
+  };
+};
 
 const ProfilePage = async () => {
   const supabase = createClient();
@@ -18,8 +25,9 @@ const ProfilePage = async () => {
   return (
     <main className="min-h-[500px] mt-10 med-small:mx-5">
       <section className="xl:!p-[0px_85px] p-[0px_55px] med-small:p-0">
+        <h1 className="text-center mb-5 text-3xl">My Account</h1>
         <ProfileInfo profile={profile} />
-        <ProfileOrderInfo />
+        <ProfileOrderInfo orders={profile.order} />
       </section>
     </main>
   );
