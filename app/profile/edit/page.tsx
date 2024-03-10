a11f9a33e5768eda3d3ca298142c4a52d09ec5bb
Alex <weiru@hotmail.ca>
2024-03-09 20:24:01 -0500

    Create edit profile page

---
+++ app/profile/edit/page.tsx
@@ -0,0 +1,26 @@
+import getProfile from "@/actions/get-profile";
+import UpdateProfileForm from "@/components/forms/profile-form";
+import { createClient } from "@/libs/supabase/server";
+import { redirect } from "next/navigation";
+
+const EditProfile = async () => {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error || !data?.user) {
+    return redirect("/account/login");
+  }
+  const profile = await getProfile(data.user.id);
+
+  return (
+    <main className="min-h-[500px] mt-10 med-small:mx-5">
+      <section className="xl:!p-[0px_85px] p-[0px_55px] med-small:p-0">
+        <h1 className="text-center mb-5 text-3xl">Edit Profile</h1>
+        <UpdateProfileForm initialData={profile} />
+      </section>
+    </main>
+  );
+};
+
+export default EditProfile;
