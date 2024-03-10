803cc1e1d008f669d99610effb31d5b7c469cbb2
Alex <weiru@hotmail.ca>
2024-03-10 19:26:18 -0400

    Make more mobile friendly

--- app/profile/components/profile-info.tsx
+++ app/profile/components/profile-info.tsx
@@ -11,12 +11,16 @@ interface EditProfileProps {
 }
 
 const ProfileInfo: React.FC<EditProfileProps> = ({ profile }) => {
+  const firstPostal = profile.postalCode.slice(0, 3);
+  const lastPostal = profile.postalCode.slice(3);
+  const postalCode = firstPostal.concat(" ", lastPostal);
+
   return (
     <div className="mb-10">
       <h1 className="text-2xl mb-3">Profile</h1>
-      <div className="flex w-1/2 bg-gray-100 rounded-[10px]">
+      <div className="flex w-1/2 bg-gray-100 rounded-[10px] med-small:w-full md-max:w-7/12">
         {/* Left Column for personal */}
-        <div className="mr-20 ml-5 my-2">
+        <div className="mr-20 ml-5 my-2 med-small:hidden">
           {/* Name */}
           <div className="mb-1">
             <h5 className={cn("text-lg", poppins_bold.className)}>Name</h5>
@@ -26,16 +30,12 @@ const ProfileInfo: React.FC<EditProfileProps> = ({ profile }) => {
           </div>
           {/* Email */}
           <div className="mb-1">
-            <h5 className={cn("text-lg", poppins_bold.className)}>
-              Email Address
-            </h5>
+            <h5 className={cn("text-lg", poppins_bold.className)}>Email</h5>
             <p>{profile.email}</p>
           </div>
           {/* Phone */}
           <div className="mb-1">
-            <h5 className={cn("text-lg", poppins_bold.className)}>
-              Phone Number
-            </h5>
+            <h5 className={cn("text-lg", poppins_bold.className)}>Phone</h5>
             <p>
               {profile.phone !== ""
                 ? `${profile.phone}`
@@ -44,7 +44,7 @@ const ProfileInfo: React.FC<EditProfileProps> = ({ profile }) => {
           </div>
         </div>
         {/* Right Column for address information */}
-        <div className="my-2 mr-6">
+        <div className="my-2 mr-6 w-full med-small:hidden">
           <h5 className={cn("text-lg", poppins_bold.className)}>Address</h5>
           {profile.address1 ? (
             <>
@@ -60,7 +60,50 @@ const ProfileInfo: React.FC<EditProfileProps> = ({ profile }) => {
             <p>No address saved.</p>
           )}
         </div>
-        <div className="mt-2 h-[24px] flex justify-end w-3/12">
+        {/* Small screen */}
+        <div className="flex flex-col ml-3 mt-2 mb-1 medium-min:hidden">
+          {/* Small screen personal info */}
+          <div className="grid grid-cols-[3fr_1fr] medium-min:hidden">
+            {/* Name */}
+            <div className="mb-2">
+              <h5 className={cn("text-lg", poppins_bold.className)}>Name</h5>
+              <p>
+                {profile.firstName} {profile.lastName}
+              </p>
+            </div>
+            {/* Phone */}
+            <div className="mb-2">
+              <h5 className={cn("text-lg", poppins_bold.className)}>Phone</h5>
+              <p>
+                {profile.phone !== ""
+                  ? `${profile.phone}`
+                  : "No phone number saved"}
+              </p>
+            </div>
+            {/* Email */}
+            <div className="mb-2">
+              <h5 className={cn("text-lg", poppins_bold.className)}>Email</h5>
+              <p>{profile.email}</p>
+            </div>
+          </div>
+          {/* Small screen for address */}
+          <div className="medium-min:hidden w-full">
+            <h5 className={cn("text-lg", poppins_bold.className)}>Address</h5>
+            {profile.address1 ? (
+              <>
+                <p>{profile.address1}</p>
+                {profile.address2 !== "" ? <p>{profile.address2}</p> : null}
+                <p>
+                  {profile.city} {profile.province}. {postalCode}
+                </p>
+                <p>{profile.country == "CA" && "Canada"}</p>
+              </>
+            ) : (
+              <p>No address saved.</p>
+            )}
+          </div>
+        </div>
+        <div className="mt-2 h-[24px] flex justify-end w-3/12 md-max:mr-2">
           <Link href="/profile/edit">
             <Edit className="hover:opacity-50" />
           </Link>
