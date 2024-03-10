dd04f7b89751904210cd4a9996b9c2e76faa1cfc
Alex <weiru@hotmail.ca>
2024-03-09 20:20:37 -0500

    Add bold poppins font

--- app/font.ts
+++ app/font.ts
@@ -10,6 +10,12 @@ export const poppins_light = Poppins({
   weight: "400",
   subsets: ["devanagari", "latin"],
 });
+
+export const poppins_bold = Poppins({
+  weight: "600",
+  subsets: ["devanagari", "latin"],
+});
+
 export const yeseva_one = Yeseva_One({
   weight: ["400"],
   subsets: ["latin", "cyrillic"],
