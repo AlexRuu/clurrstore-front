e30820ff6a451551f6b3ff63444ef11325d6fe14
Alex <weiru@hotmail.ca>
2024-03-16 20:10:43 -0400

    Update styling

--- app/delivery/page.tsx
+++ app/delivery/page.tsx
@@ -14,8 +14,8 @@ const DeliveryPage = () => {
     <main className="min-h-[500px] mt-10 pb-[30px] small:mt-0 xsmall:mt-0">
       <section className="xl:!p-[0px_85px] p-[0px_55px] med-small:p-0">
         <PageHeader headerTitle="Delivery Policy" second="delivery" />
-        <div className="flex justify-center items-center text-left w-full">
-          <p className={cn("mx-32 w-1/3", poppins_light)}>
+        <div className="flex justify-center items-center w-full">
+          <p className={cn("mx-32 w-1/2 text-center text-xl", poppins_light)}>
             Delivery times vary depending on your selected delivery address,
             availability of your items, and the time of the day you place your
             order.
