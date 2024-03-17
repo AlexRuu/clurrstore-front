5652ecb34640f5bdf985d280a5a5ab94112acb5c
Alex <weiru@hotmail.ca>
2024-03-16 20:10:50 -0400

    Fix key error

--- components/ui/breadcrumbs.tsx
+++ components/ui/breadcrumbs.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/libs/utlils";
+import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -27,27 +28,32 @@ const Breadcrumb: React.FC<BreadcrumbProps> = ({ second, title, disabled }) => {
     },
   ];
   return (
-    <div className="">
+    <div className="items-center flex">
       {links.map((link, index) => (
-        <Link
-          href={link.href}
-          key={index}
-          aria-disabled={link.disabled ? link.disabled : false}
-          tabIndex={link.disabled ? -1 : undefined}
-          className={cn(
-            " text-gray-500 transition ease-out duration-150 hover:text-neutral-400 uppercase",
-            link.active && "text-black",
-            link.disabled && "pointer-events-none"
+        <div className="flex items-center" key={index}>
+          <Link
+            href={link.href}
+            aria-disabled={link.disabled ? link.disabled : false}
+            tabIndex={link.disabled ? -1 : undefined}
+            className={cn(
+              " text-gray-500 transition ease-out duration-150 hover:text-neutral-400 uppercase",
+              link.active && "text-black",
+              link.disabled && "pointer-events-none"
+            )}
+          >
+            {link.label}
+          </Link>
+          {index == 0 && (
+            <span className="mx-1" key={index}>
+              <ChevronRight size={13} />
+            </span>
           )}
-        >
-          {link.label}
-          {index == 0 && <span className="mr-2 ml-2">/</span>}
-        </Link>
+        </div>
       ))}
       <>
         {title && (
           <>
-            <span className="ml-2 mr-2">/</span>
+            <span className="ml-2 mr-2">{">"}</span>
             <p className="inline uppercase">{title}</p>
           </>
         )}
