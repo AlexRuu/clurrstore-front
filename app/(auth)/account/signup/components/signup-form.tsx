75fb65efcdb6fe733284bc77e2f5ec5f460f6bec
Alex <weiru@hotmail.ca>
2024-03-20 19:38:34 -0400

    Add loading

--- app/(auth)/account/signup/components/signup-form.tsx
+++ app/(auth)/account/signup/components/signup-form.tsx
@@ -16,8 +16,11 @@ import { Input } from "@/components/ui/input";
 import Button from "@/components/ui/myButton";
 import Link from "next/link";
 import { signup } from "@/actions/auth";
+import { useState } from "react";
+import SignupLoader from "./signup-loader";
 
 const SignUpForm = () => {
+  const [loading, setLoading] = useState(false);
   const formSchema = z
     .object({
       firstName: z.string().min(1),
@@ -48,129 +51,139 @@ const SignUpForm = () => {
   });
 
   const onSignup = async (data: SignUpFormValues) => {
-    await signup(data.firstName, data.lastName, data.email, data.password);
+    setLoading(true);
+    try {
+      await signup(data.firstName, data.lastName, data.email, data.password);
+    } catch (error) {
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSignup)}
-        method="post"
-        autoComplete="off"
-        className="w-full justify-center items-center"
-      >
-        <div className="w-2/5 mx-auto flex flex-col">
-          <div className="flex w-full">
-            <FormField
-              control={form.control}
-              name="firstName"
-              render={({ field }) => (
-                <FormItem className="w-1/2 mr-2">
-                  <FormLabel>First Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="text"
-                      autoComplete="off"
-                      autoCapitalize="none"
-                      value={field.value}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="lastName"
-              render={({ field }) => (
-                <FormItem className="w-1/2 ml-2">
-                  <FormLabel>Last Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="text"
-                      autoComplete="off"
-                      autoCapitalize="none"
-                      value={field.value}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+    <>
+      {loading && <SignupLoader />}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSignup)}
+          method="post"
+          autoComplete="off"
+          className="w-full justify-center items-center"
+        >
+          <div className="w-2/5 mx-auto flex flex-col">
+            <div className="flex w-full">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem className="w-1/2 mr-2">
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="text"
+                        autoComplete="off"
+                        autoCapitalize="none"
+                        value={field.value}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem className="w-1/2 ml-2">
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="text"
+                        autoComplete="off"
+                        autoCapitalize="none"
+                        value={field.value}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        autoComplete="off"
+                        autoCapitalize="none"
+                        value={field.value}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        autoComplete="off"
+                        autoCapitalize="none"
+                        value={field.value}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        autoComplete="off"
+                        autoCapitalize="none"
+                        value={field.value}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="text-center">
+              <Button className="mr-2 text-black small:w-full small:text-center w-full my-5 small:px-0 border bg-[#e2ecf2] hover:shadow-home-button hover:-translate-y-[3px] hover:brightness-95 uppercase">
+                Sign Up
+              </Button>
+            </div>
+            <div className="flex text-center mx-auto pt-4">
+              <p className="mr-1">Already a member?</p>
+              <Link href="/account/login" className="text-blue-500">
+                Sign In Here
+              </Link>
+            </div>
           </div>
-          <div className="flex flex-col">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="email"
-                      autoComplete="off"
-                      autoCapitalize="none"
-                      value={field.value}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="password"
-                      autoComplete="off"
-                      autoCapitalize="none"
-                      value={field.value}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="confirmPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Confirm Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="password"
-                      autoComplete="off"
-                      autoCapitalize="none"
-                      value={field.value}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="text-center">
-            <Button className="mr-2 text-black small:w-full small:text-center w-full my-5 small:px-0 border bg-[#e2ecf2] hover:shadow-home-button hover:-translate-y-[3px] hover:brightness-95 uppercase">
-              Sign Up
-            </Button>
-          </div>
-          <div className="flex text-center mx-auto pt-4">
-            <p className="mr-1">Already a member?</p>
-            <Link href="/account/login" className="text-blue-500">
-              Sign In Here
-            </Link>
-          </div>
-        </div>
-      </form>
-    </Form>
+        </form>
+      </Form>
+    </>
   );
 };
 
