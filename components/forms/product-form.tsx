238fa2325d8fb62126628427436b4adf877c6aad
Alex <weiru@hotmail.ca>
2024-03-23 16:10:03 -0400

    Change spacing of button

--- components/forms/product-form.tsx
+++ components/forms/product-form.tsx
@@ -207,7 +207,7 @@ const ProductForm: React.FC<ProductFormProps> = ({
         ) : (
           <Button
             type="submit"
-            className="text-black small:text-center border bg-[#e2ecf2] hover:shadow-home-button hover:-translate-y-[3px] hover:brightness-95 uppercase w-full"
+            className="text-black mt-8 small:text-center border bg-[#e2ecf2] hover:shadow-home-button hover:-translate-y-[3px] hover:brightness-95 uppercase w-full"
           >
             Add to Cart
           </Button>
